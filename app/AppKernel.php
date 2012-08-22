337e400e96b32a6c31448607da8859c92b8236ea
Javier Martinez Fernandez <ecentinela@gmail.com>
2012-08-22 20:11:48 +0200

    Added a new bundle to set base url (cli application don't have it).

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
             new Bazinga\ExposeTranslationBundle\BazingaExposeTranslationBundle(),
+            new Frosas\BaseUrlBundle\FrosasBaseUrlBundle(),
             new Ecentinela\ComiconvBundle\EcentinelaComiconvBundle(),
         );
 
